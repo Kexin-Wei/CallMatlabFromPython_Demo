1b0fd0c4c54e34dcb453704b8bbc003a94ac0f6b
KK <vacationwkx@gmail.com>
2025-05-16 17:21:13 +0800

    update test simulink file

--- test_simulink.py
+++ test_simulink.py
@@ -1,4 +1,6 @@
 import time
+import matlab.engine
+
 
 model_name = 'test'
 
@@ -9,42 +11,68 @@ def sim_to_end(eng, model_name):
 
 # Type 2
 class TestSimulink:
-    def __init__(self, eng, model_name):
-        self.eng = eng
+    def __init__(self, ):
+        self.eng = None
         self.model_name = model_name
         self.yHistory = 0
         self.tHistory = 0
+
+    def __del__(self):
+        if self.eng is not None:
+            self._simulink_stop()
+            self.eng.quit()
+            print("Disconnected from Matlab")
+
+    def _simulink_set_u(self, u):
+        self.eng.set_param(f'{self.model_name}/u','value',str(u),nargout=0)
+
+    def _simulink_start_and_pause(self):
+        self.eng.set_param(self.model_name, 'SimulationCommand', 'start', nargout=0)
+        self.eng.set_param(self.model_name, 'SimulationCommand', 'pause', nargout=0)
+
+    def _simulink_step(self):
+        self.eng.set_param(self.model_name, 'SimulationCommand', 'continue', nargout=0)
+        self.eng.set_param(self.model_name, 'SimulationCommand', 'pause', nargout=0)
+    
+    def _simulink_stop(self):
+        self.eng.set_param(self.model_name, 'SimulationCommand', 'stop', nargout=0)
     
     def start(self):
-        self.eng
+        self.eng = matlab.engine.start_matlab()
+        print("Connected to Matlab")
+        self.eng.addpath(r'./matlab')
+        self.eng.eval(f"model = '{self.model_name}';", nargout=0)
+        self.eng.eval(f"load_system(model)", nargout=0)
+
+        # initialize 
+        self._simulink_set_u(0)
+        # get parameters
+
+        # start the simulation and pause
+        self._simulink_start_and_pause()
+        print("Simulation started and paused")
+
+        self._update_history()
     
-    def update_history(self):
+    def _update_history(self):
         # Get the simulation history
-        self.yHistory = self.eng.workspace['simout']
-        self.tHistory = self.eng.workspace['tout']
-        
+        self.yHistory = self.eng.workspace['out'].simout
+        self.tHistory = self.eng.workspace['out'].time
 
-    def open(self):
-        self.eng.open_system(self.model_name, nargout=0)
-
-    def close(self):
-        self.eng.close_system(self.model_name, nargout=0)
-
-    def sim(self):
-        self.eng.sim(self.model_name)
-
-
+    def step(self, u):
+        if (self.eng.get_param(self.modelName,'SimulationStatus') != ('stopped' or 'terminating')):
+            self._simulink_set_u(u)
+            self._simulink_step()
+            self._update_history()
+            print(f"t: {self.tHistory}, y: {self.yHistory}")
 
 def _test(eng):
     # This is a test function to check if the MATLAB engine is working
     # put your test code here
     eng.addpath(r'./matlab')
     sim_to_end(eng, model_name)
-    
-
 
 def matlab_engine_test():
-    import matlab.engine
     eng = matlab.engine.start_matlab()
     tic = time.time()
     _test(eng)
@@ -53,10 +81,24 @@ def matlab_engine_test():
     eng.quit()
     return eng
 
-
-
-if __name__ == "__main__":
+def type1_test():
     tic = time.time()
     matlab_engine_test()
     tic = time.time() - tic
     print(f"External elapased time: {tic:.2f} seconds")
+
+def type2_test():
+    tic = time.time()
+    test_simulink = TestSimulink()
+    test_simulink.start()
+    print("Simulation started")
+    for i in range(10):
+        print(f"Step {i+1}")
+        test_simulink.step(i)
+        time.sleep(0.1)
+    tic = time.time() - tic
+    print(f"External elapased time: {tic:.2f} seconds")
+
+if __name__ == "__main__":
+    # type1_test()
+    type2_test()
