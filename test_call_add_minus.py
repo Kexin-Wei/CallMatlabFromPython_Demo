f485f37f4027757ad058c056cac18ada6b85b70a
KK <vacationwkx@gmail.com>
2025-05-16 13:50:08 +0800

    test_matlab_internal

--- test_call_add_minus.py
+++ test_call_add_minus.py
@@ -1,9 +1,5 @@
 import time
 
-def get_matlab_engine():
-    import matlab.engine
-    eng = matlab.engine.start_matlab()
-    return eng
 
 def test_add_minus(eng):
     eng.addpath(r'./matlab')
@@ -11,9 +7,27 @@ def test_add_minus(eng):
     print(a,b)
 
 
+def _test(eng):
+    # This is a test function to check if the MATLAB engine is working
+    # put your test code here
+    test_add_minus(eng)
+
+
+def matlab_engine_test():
+    import matlab.engine
+    eng = matlab.engine.start_matlab()
+    tic = time.time()
+    _test(eng)
+    tic = time.time() - tic
+    print(f"Internal elapased time: {tic:.2f} seconds")
+    eng.quit()
+    return eng
+
+
+
 if __name__ == "__main__":
     tic = time.time()
-    test_add_minus(get_matlab_engine())
+    matlab_engine_test()
     tic = time.time() - tic
-    print(f"Elapsed time: {tic:.2f} seconds")
+    print(f"External elapased time: {tic:.2f} seconds")
     # eng.quit()cd 
